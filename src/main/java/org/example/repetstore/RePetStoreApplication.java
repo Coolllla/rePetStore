2496ccf2a964dcb969447c8b67b2a672fa95ce11
Coolllla <1320269132@qq.com>
2025-03-10 14:14:36 +0800

    init program

---
+++ src/main/java/org/example/repetstore/RePetStoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.repetstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RePetStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RePetStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/repetstore/RePetStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.repetstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RePetStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
